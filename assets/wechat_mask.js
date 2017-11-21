d0f6a0c28dea60e9bd98488cc94ae6e5a2fd8e2f
erabbit <rabbitom@gmail.com>
2017-11-21 20:55:48 +0800

    完善遮罩和跳转URL

--- assets/wechat_mask.js
+++ assets/wechat_mask.js
@@ -1,4 +1,8 @@
 function checkForWechat() {
+    function is_ios() {
+        return (navigator.userAgent.indexOf('iPhone') > -1);
+    }
+
     function is_weixin() {
         var ua = navigator.userAgent.toLowerCase();
         if (ua.match(/MicroMessenger/i) == "micromessenger") {
@@ -9,7 +13,9 @@ function checkForWechat() {
     }
     var isWeixin = is_weixin();
     if (isWeixin) {
+        if (is_ios())
+            document.getElementById("system-browser").innerHTML = "<img class='browser-icon' src='assets/safari_icon_small.png'/>Safari";
         document.getElementById("mask").style.display = "block";
-        document.getElementById("top").style.display = "block";
+        document.getElementById("prompt").style.display = "block";
     }
 }
